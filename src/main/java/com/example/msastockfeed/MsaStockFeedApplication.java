e2bc82ea438b17ce2d7357cd70f40c85739c4f7b
bomsprnn <qhm12140525@gmail.com>
2024-04-25 23:22:43 +0900

    feat ) update module

---
+++ src/main/java/com/example/msastockfeed/MsaStockFeedApplication.java
@@ -0,0 +1,14 @@
+package com.example.msastockfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class MsaStockFeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaStockFeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/msastockfeed/MsaStockFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msastockfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaStockFeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
